8d9cfddbaa412915f271ec3b6f2f940cac97248a
FischSc-maker <afernandoveiga@gmail.com>
2025-01-31 19:51:13 -0300

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,375 @@
+--[=[
+ d888b  db    db d888888b      .d888b.      db      db    db  .d8b.  
+88' Y8b 88    88   `88'        VP  `8D      88      88    88 d8' `8b 
+88      88    88    88            odD'      88      88    88 88ooo88 
+88  ooo 88    88    88          .88'        88      88    88 88~~~88 
+88. ~8~ 88b  d88   .88.        j88.         88booo. 88b  d88 88   88    @uniquadev
+ Y888P  ~Y8888P' Y888888P      888888D      Y88888P ~Y8888P' YP   YP  CONVERTER 
+]=]
+
+-- Instances: 21 | Scripts: 2 | Modules: 0 | Tags: 0
+local G2L = {};
+
+-- StarterGui.NSNndnjdnf
+G2L["1"] = Instance.new("ScreenGui", game:GetService("CoreGui");
+G2L["1"]["Name"] = [[NSNndnjdnf]];
+G2L["1"]["ZIndexBehavior"] = Enum.ZIndexBehavior.Sibling;
+
+
+-- StarterGui.NSNndnjdnf.Frame
+G2L["2"] = Instance.new("Frame", G2L["1"]);
+G2L["2"]["BorderSizePixel"] = 0;
+G2L["2"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["2"]["Size"] = UDim2.new(0.20828, 0, 0.02772, 0);
+G2L["2"]["Position"] = UDim2.new(0.17189, 0, 0.18124, 0);
+G2L["2"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["2"]["BackgroundTransparency"] = 1;
+
+
+-- StarterGui.NSNndnjdnf.Frame.Frame
+G2L["3"] = Instance.new("Frame", G2L["2"]);
+G2L["3"]["BorderSizePixel"] = 0;
+G2L["3"]["BackgroundColor3"] = Color3.fromRGB(41, 41, 41);
+G2L["3"]["Size"] = UDim2.new(1.13855, 0, 17.92308, 0);
+G2L["3"]["Position"] = UDim2.new(0.01205, 0, -0.15385, 0);
+G2L["3"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+
+
+-- StarterGui.NSNndnjdnf.Frame.Frame.UICorner
+G2L["4"] = Instance.new("UICorner", G2L["3"]);
+
+
+
+-- StarterGui.NSNndnjdnf.Frame.Frame.e
+G2L["5"] = Instance.new("TextBox", G2L["3"]);
+G2L["5"]["CursorPosition"] = -1;
+G2L["5"]["Name"] = [[e]];
+G2L["5"]["PlaceholderColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["5"]["ZIndex"] = 5;
+G2L["5"]["BorderSizePixel"] = 0;
+G2L["5"]["TextWrapped"] = true;
+G2L["5"]["TextSize"] = 14;
+G2L["5"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["5"]["BackgroundColor3"] = Color3.fromRGB(76, 76, 76);
+G2L["5"]["RichText"] = true;
+G2L["5"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["5"]["PlaceholderText"] = [[5]];
+G2L["5"]["Size"] = UDim2.new(0.82011, 0, 0.1588, 0);
+G2L["5"]["Position"] = UDim2.new(0.08995, 0, 0.29614, 0);
+G2L["5"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["5"]["Text"] = [[]];
+G2L["5"]["BackgroundTransparency"] = 1;
+
+
+-- StarterGui.NSNndnjdnf.Frame.Frame.e.UIStroke
+G2L["6"] = Instance.new("UIStroke", G2L["5"]);
+G2L["6"]["ApplyStrokeMode"] = Enum.ApplyStrokeMode.Border;
+
+
+-- StarterGui.NSNndnjdnf.Frame.Frame.e.UICorner
+G2L["7"] = Instance.new("UICorner", G2L["5"]);
+
+
+
+-- StarterGui.NSNndnjdnf.Frame.Frame.start
+G2L["8"] = Instance.new("TextBox", G2L["3"]);
+G2L["8"]["CursorPosition"] = -1;
+G2L["8"]["Name"] = [[start]];
+G2L["8"]["PlaceholderColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["8"]["ZIndex"] = 5;
+G2L["8"]["BorderSizePixel"] = 0;
+G2L["8"]["TextWrapped"] = true;
+G2L["8"]["TextSize"] = 14;
+G2L["8"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["8"]["BackgroundColor3"] = Color3.fromRGB(76, 76, 76);
+G2L["8"]["RichText"] = true;
+G2L["8"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["8"]["PlaceholderText"] = [[1]];
+G2L["8"]["Size"] = UDim2.new(0.82011, 0, 0.1588, 0);
+G2L["8"]["Position"] = UDim2.new(0.08995, 0, 0.1073, 0);
+G2L["8"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["8"]["Text"] = [[]];
+G2L["8"]["BackgroundTransparency"] = 1;
+
+
+-- StarterGui.NSNndnjdnf.Frame.Frame.start.UIStroke
+G2L["9"] = Instance.new("UIStroke", G2L["8"]);
+G2L["9"]["ApplyStrokeMode"] = Enum.ApplyStrokeMode.Border;
+
+
+-- StarterGui.NSNndnjdnf.Frame.Frame.start.UICorner
+G2L["a"] = Instance.new("UICorner", G2L["8"]);
+
+
+
+-- StarterGui.NSNndnjdnf.Frame.Frame.fnl
+G2L["b"] = Instance.new("TextBox", G2L["3"]);
+G2L["b"]["CursorPosition"] = -1;
+G2L["b"]["Name"] = [[fnl]];
+G2L["b"]["PlaceholderColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["b"]["ZIndex"] = 5;
+G2L["b"]["BorderSizePixel"] = 0;
+G2L["b"]["TextWrapped"] = true;
+G2L["b"]["TextSize"] = 14;
+G2L["b"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["b"]["BackgroundColor3"] = Color3.fromRGB(76, 76, 76);
+G2L["b"]["RichText"] = true;
+G2L["b"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["b"]["PlaceholderText"] = [[!]];
+G2L["b"]["Size"] = UDim2.new(0.82011, 0, 0.1588, 0);
+G2L["b"]["Position"] = UDim2.new(0.08995, 0, 0.48069, 0);
+G2L["b"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["b"]["Text"] = [[]];
+G2L["b"]["BackgroundTransparency"] = 1;
+
+
+-- StarterGui.NSNndnjdnf.Frame.Frame.fnl.UIStroke
+G2L["c"] = Instance.new("UIStroke", G2L["b"]);
+G2L["c"]["ApplyStrokeMode"] = Enum.ApplyStrokeMode.Border;
+
+
+-- StarterGui.NSNndnjdnf.Frame.Frame.fnl.UICorner
+G2L["d"] = Instance.new("UICorner", G2L["b"]);
+
+
+
+-- StarterGui.NSNndnjdnf.Frame.Frame.str
+G2L["e"] = Instance.new("TextButton", G2L["3"]);
+G2L["e"]["BorderSizePixel"] = 0;
+G2L["e"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["e"]["TextSize"] = 14;
+G2L["e"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["e"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Bold, Enum.FontStyle.Italic);
+G2L["e"]["Size"] = UDim2.new(0.82011, 0, 0.14592, 0);
+G2L["e"]["BackgroundTransparency"] = 1;
+G2L["e"]["Name"] = [[str]];
+G2L["e"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["e"]["Text"] = [[Start]];
+G2L["e"]["Position"] = UDim2.new(0.08995, 0, 0.80687, 0);
+
+
+-- StarterGui.NSNndnjdnf.Frame.Frame.str.UIStroke
+G2L["f"] = Instance.new("UIStroke", G2L["e"]);
+G2L["f"]["ApplyStrokeMode"] = Enum.ApplyStrokeMode.Border;
+
+
+-- StarterGui.NSNndnjdnf.Frame.Frame.str.UICorner
+G2L["10"] = Instance.new("UICorner", G2L["e"]);
+
+
+
+-- StarterGui.NSNndnjdnf.Frame.Frame.LocalScript
+G2L["11"] = Instance.new("LocalScript", G2L["3"]);
+
+
+
+-- StarterGui.NSNndnjdnf.Frame.Frame.close
+G2L["12"] = Instance.new("TextButton", G2L["3"]);
+G2L["12"]["TextWrapped"] = true;
+G2L["12"]["BorderSizePixel"] = 0;
+G2L["12"]["TextColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["12"]["TextSize"] = 14;
+G2L["12"]["BackgroundColor3"] = Color3.fromRGB(255, 0, 0);
+G2L["12"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Bold, Enum.FontStyle.Italic);
+G2L["12"]["Size"] = UDim2.new(0.18519, 0, 0.14163, 0);
+G2L["12"]["Name"] = [[close]];
+G2L["12"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["12"]["Text"] = [[X]];
+G2L["12"]["Position"] = UDim2.new(0.91005, 0, -0.08584, 0);
+
+
+-- StarterGui.NSNndnjdnf.Frame.Frame.close.UIStroke
+G2L["13"] = Instance.new("UIStroke", G2L["12"]);
+G2L["13"]["ApplyStrokeMode"] = Enum.ApplyStrokeMode.Border;
+
+
+-- StarterGui.NSNndnjdnf.Frame.Frame.close.UICorner
+G2L["14"] = Instance.new("UICorner", G2L["12"]);
+
+
+
+-- StarterGui.NSNndnjdnf.Frame.UIDrag
+G2L["15"] = Instance.new("LocalScript", G2L["2"]);
+G2L["15"]["Name"] = [[UIDrag]];
+
+
+-- StarterGui.NSNndnjdnf.Frame.Frame.LocalScript
+local function C_11()
+local script = G2L["11"];
+	-- Função para converter número em extenso
+	
+	local fnl = "!"
+	
+	local unidades = {
+		"UM", "DOIS", "TRÊS", "QUATRO", 
+		"CINCO", "SEIS", "SETE", "OITO", "NOVE"
+	}
+	local dezenas = {
+		[10] = "DEZ", [11] = "ONZE", [12] = "DOZE", [13] = "TREZE", 
+		[14] = "CATORZE", [15] = "QUINZE", [16] = "DEZESSEIS", 
+		[17] = "DEZESSETE", [18] = "DEZOITO", [19] = "DEZENOVE"
+	}
+	local dezenasCheias = {
+		"DEZ", "VINTE", "TRINTA", "QUARENTA", 
+		"CINQUENTA", "SESSENTA", "SETENTA", 
+		"OITENTA", "NOVENTA"
+	}
+	local centenasCheias = {
+		"CEM", "CENTO", "DUZENTOS", "TREZENTOS", "QUATROCENTOS",
+		"QUINHENTOS", "SEISCENTOS", "SETECENTOS",
+		"OITOCENTOS", "NOVECENTOS"
+	}
+	
+	-- Função principal para converter número em extenso
+	local function npr(n, mil)
+		if n < 0 or n > 100000 then
+			return "Número fora do alcance suportado (0 a 100.000)"
+		end
+	
+		if n == 0 then return "ZERO"..fnl end
+	
+		local resultado = ""
+	
+		-- Milhares
+		local milhares = math.floor(n / 1000)
+		n = n % 1000
+		if milhares > 0 then
+			if milhares == 1 then
+				resultado = "MIL"
+			else
+				resultado = npr(milhares, true) .. " MIL"
+			end
+		end
+	
+		-- Centenas
+		local centenas = math.floor(n / 100)
+		n = n % 100
+		if centenas > 0 then
+			if centenas == 1 and n == 0 then
+				resultado = resultado .. " " .. centenasCheias[1]
+			else
+				resultado = resultado .. " " .. centenasCheias[centenas+1]
+			end
+		end
+	
+		-- Dezenas e unidades
+		if n > 0 then
+			if n >= 10 and n <= 19 then
+				resultado = resultado .. " " .. dezenas[n]
+			else
+				local dezena = math.floor(n / 10)
+				local unidade = n % 10
+				if dezena > 0 then
+					if resultado ~= "" then
+						resultado = resultado .. " E " .. dezenasCheias[dezena]
+					end
+					if resultado == "" then
+					resultado = resultado .. " " .. dezenasCheias[dezena]
+					end
+					
+				end
+				if unidade > 0 and n > 0 then
+					if resultado ~= "" then
+						resultado = resultado .. " E " .. unidades[unidade]
+					end
+					if resultado == "" then
+						resultado = unidades[unidade]
+					end
+					
+				end
+			end
+		end
+		if mil == true then
+		resultado = resultado..fnl
+		end
+		
+		if resultado:find("!") then
+			resultado = resultado:gsub("!", "")
+		end
+		
+		resultado = resultado..fnl
+	
+		return resultado:gsub("^%s+", "") 
+	end
+	
+	-- Testando a função
+	
+	
+	-- Função para contar até o número com espera de 0.2 segundos
+	function can(start, maxNumero)
+		for i = start, maxNumero do
+			wait(1)
+			local extenso = npr(i)
+			game.TextChatService.TextChannels.RBXGeneral:SendAsync(extenso)
+			pcall(function(...) 
+				keypress(Enum.KeyCode.Space)
+			end)
+		end
+	end
+	
+	repeat wait() until game.TextChatService.TextChannels.RBXGeneral
+	
+	
+	local fr = script.Parent
+	local s = fr.str
+	local c = fr.close
+	local f = fr.fnl
+	local ss = fr.start
+	local e = fr.e
+	
+	s.MouseButton1Click:Connect(function() 
+		if ss.Text == "" then return end
+		if e.Text == "" then return end
+		
+		can(ss.Text, e.Text)
+	end)
+	
+	f:GetPropertyChangedSignal("Text"):Connect(function(...: any) 
+		fnl = f.Text
+	end)
+end;
+task.spawn(C_11);
+-- StarterGui.NSNndnjdnf.Frame.UIDrag
+local function C_15()
+local script = G2L["15"];
+	-- Made by Real_IceyDev (@lceyDex) --
+	-- Simple UI dragger (PC Only/Any device that has a mouse) --
+	
+	local UIS = game:GetService('UserInputService')
+	local frame = script.Parent
+	local dragToggle = nil
+	local dragSpeed = 0.25
+	local dragStart = nil
+	local startPos = nil
+	
+	local function updateInput(input)
+		local delta = input.Position - dragStart
+		local position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X,
+			startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+		game:GetService('TweenService'):Create(frame, TweenInfo.new(dragSpeed), {Position = position}):Play()
+	end
+	
+	frame.InputBegan:Connect(function(input)
+		if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then 
+			dragToggle = true
+			dragStart = input.Position
+			startPos = frame.Position
+			input.Changed:Connect(function()
+				if input.UserInputState == Enum.UserInputState.End then
+					dragToggle = false
+				end
+			end)
+		end
+	end)
+	
+	UIS.InputChanged:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+			if dragToggle then
+				updateInput(input)
+			end
+		end
+	end)
+end;
+task.spawn(C_15);
+
+return G2L["1"], require;
